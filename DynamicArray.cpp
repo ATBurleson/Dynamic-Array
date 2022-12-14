40db08eb9ca00f7667c015d9316ad5f34d467a61
Austin <115430338+ATBurleson@users.noreply.github.com>
2022-12-14 16:40:13 -0600

    Update DynamicArray.cpp

--- DynamicArray.cpp
+++ DynamicArray.cpp
@@ -46,10 +46,16 @@ int main()
 	//if the user presses 'ENTER' after the program is finished, we deallocated the memory used for the dynamic array 
 	//and set back equal to 'NULL' or zero.  con.ignore() function can be used the wait for the 'ENTER' key to be pressed
 	//but you can also use cin.get().
-	if (cin.ignore())
+	cout << endl << endl;
+	if (DArray)
 	{
-		delete[] DArray;
-		DArray = NULL;
+		cout << "Array is full, press ENTER to clear!!" << endl;
+		if (cin.ignore())
+		{
+			delete[] DArray;
+			DArray = NULL;
+		}
+		
 	}
 }
 
